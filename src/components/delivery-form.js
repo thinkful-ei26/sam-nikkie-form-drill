49e9e8f57e50bbfa07b8b3dcaa5d4401a1d77df5
Nikkie Mashian <nikkiemashian@gmail.com>
2018-12-19 16:18:23 -0800

    refactored with actions

--- src/components/delivery-form.js
+++ src/components/delivery-form.js
@@ -3,56 +3,12 @@ import './delivery-form.css'
 import {reduxForm, Field, SubmissionError, focus} from 'redux-form';
 import {required, nonEmpty, five, num, sure} from '../validators';
 import Input from './input';
+import { fetchDelivery } from '../actions';
 
 
 export class DeliveryForm extends React.Component{
   onSubmit(values) {
-    return fetch('https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
-        method: 'POST',
-        body: JSON.stringify(values),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(res => {
-            if (!res.ok) {
-                if (
-                    res.headers.has('content-type') &&
-                    res.headers
-                        .get('content-type')
-                        .startsWith('application/json')
-                ) {
-                    // It's a nice JSON error returned by the server, so decode it
-                    console.log('its a nice JSON error');
-                    return res.json().then(err => Promise.reject(err));
-                }
-                // It's a less informative error returned by express
-                console.log('its less informative :(');
-                return Promise.reject({
-                    code: res.status,
-                    message: res.statusText
-                });
-            }
-            return;
-        })
-        .then(() => console.log('Submitted with values', values))
-        .catch(err => {
-            console.log('the err is', err);
-            const {reason, message, location} = err;
-            if (reason === 'ValidationError') {
-                // Convert ValidationErrors into SubmissionErrors for Redux Form
-                return Promise.reject(
-                    new SubmissionError({
-                        [location]: message
-                    })
-                );
-            }
-            return Promise.reject(
-                new SubmissionError({
-                    _error: err.message
-                })
-            );
-        });
+    this.props.dispatch(fetchDelivery(values)); 
 }
 
   render(){
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
-import {createStore, combineReducers} from 'redux'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {reducer as formReducer} from 'redux-form'
+import thunk from 'redux-thunk'
 
 export default createStore(
     combineReducers({
         form: formReducer
-    })
+    }),
+    applyMiddleware(thunk)
 );
